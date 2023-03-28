a9c4dd68c75049ff58c580bcba8da2f3d7d4cccb
amcginn92 <a.mcginn92@gmail.com>
2023-03-28 12:06:21 -0400

    Finished

--- app/src/main/java/com/example/coroutinesinclass/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesinclass/MainActivity.kt
@@ -6,17 +6,19 @@ import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
 
 
-    val timerHandler = Handler(Looper.getMainLooper()){
-        countDownTimer.text = it.what.toString()
-
-        true
-    }
+//    val timerHandler = Handler(Looper.getMainLooper()){
+//        countDownTimer.text = it.what.toString()
+//
+//        true
+//    }
     val countDownTimer: TextView by lazy{
         findViewById(R.id.countDownTimer)
     }
@@ -26,24 +28,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button)
 
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
 
-
-        Thread{
-            for(i in 100 downTo 1){
-                Log.d("Countdown", i.toString())
-                Thread.sleep(1000)
-//                val msg = Message.obtain()  //obtain from pool
-//
-//                msg.what = i    //put information into object
-//
-//                timerHandler.sendMessage(msg)   //send the handler
-//                //puts on main thread
-                timerHandler.sendEmptyMessage(i)    //shorthand
+        button.setOnClickListener{
+            scope.launch{
+                countdownTimer()
             }
-        }.start()
+
+
+        }
 
 
 
+
+
+
+
+//        Thread{
+//            for(i in 100 downTo 1){
+//                Log.d("Countdown", i.toString())
+//                Thread.sleep(1000)
+////                val msg = Message.obtain()  //obtain from pool
+////
+////                msg.what = i    //put information into object
+////
+////                timerHandler.sendMessage(msg)   //send the handler
+////                //puts on main thread
+//                timerHandler.sendEmptyMessage(i)    //shorthand
+//            }
+//        }.start()
+
+
+
+    }
+    suspend fun countdownTimer(){
+        repeat(100){
+
+            (100-it).toString().run{
+                withContext(Dispatchers.Main){
+                    countDownTimer.text = this@run
+                }
+                delay(1000)
+            }
+
+        }
     }
 }
